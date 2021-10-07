625b6d4105a9068b82e2b722ca4f3b51e58bb31c
mikpa1337 <44301633+mikpa1337@users.noreply.github.com>
2021-10-07 18:17:09 +0300

    Working example? (test)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <cstdlib>  // std::exit
+#include <GLFW/glfw3.h>
+
 
 int main() {
     std::cout << "hello, world\nalso hello cmake\n" << std::endl;
+
+    GLFWwindow *window = nullptr;
+    int width = 640;
+    int height = 480;
+
+    if (!glfwInit()){
+        std::cerr << "lol no glfw init 4 u\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    window = glfwCreateWindow(width, height, "test", nullptr, nullptr);
+    if (!window){
+        std::cerr << "lol no window 4 u (VITTU)" << std::endl;
+        glfwTerminate();
+        std::exit(EXIT_FAILURE);
+    }
+    
+    while (!glfwWindowShouldClose(window)){
+        // draw
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // mr kill
+    glfwTerminate();
     return 0;
 }
